f5673e436048f87ef20dc78bbcbc9d6d1167aa46
Sohrab <sohrab.taee@gmail.com>
2017-09-14 00:08:52 +0430

    First version

---
+++ src/store/configureStore.js
@@ -0,0 +1,18 @@
+import {createStore, applyMiddleware, compose} from 'redux';
+// To handle async actions (like API calls) in store
+import thunk from 'redux-thunk';
+// Reducers
+import reducers from '../reducers';
+// Initial state
+import initialState from './initialState';
+
+const configureStore = () => {
+    return createStore(
+        reducers,
+        initialState,
+        // Apply thunk middleware and add support for Redux dev tools in Google Chrome
+        compose(applyMiddleware(thunk), window.devToolsExtension && window.devToolsExtension())
+    );
+};
+
+export default configureStore;
